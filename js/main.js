b6193f7014c66ab007fe61e460846c6d1b9b608e
Cody Breen <codybreen@Codys-MacBook-Air.local>
2019-12-09 01:36:50 -0500

    Tried everything, can't get X button to work. Last commit before submission.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 $('#button-add-skill').click(function() {
+    console.log("clicked the add button");
     let skillEntered = $('#input-add-skill').val();
     let list = $('tbody');
-    let lineFormat = `<tr><td class="list-items"><button class="button-remove-list-item">X</button>${skillEntered}</td><br/></tr>`;
+    let lineFormat = `<tr><td class="list-items"><input type="submit" id="button-remove" value="X">${skillEntered}</td><br/></tr>`;
     list.append(lineFormat);
 });
 
 
-$('.button-remove-list-item').click(function() {
-    let lineToRemove = $('.list-items').closest;
-    console.log(lineToRemove.val());
-    lineToRemove.remove();
-});
+
+// this is code for the cancel button. I cannot get it to work but 
+// would like to know how to get it to work. I've tried everything.
+// $('.list-items').ready(function() {
+//     console.log("ready");
+//     $('#button').click(function() {
+//     console.log("clicked x")
+//         $(this).closest('tr').remove();
+
+//     });
+// });
+
+
 
 
