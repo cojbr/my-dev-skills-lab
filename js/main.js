8be08f2be336fd6ced9edd996a15c915302d4a73
Cody Breen <codybreen@Codys-MacBook-Air.local>
2019-12-09 00:16:43 -0500

    Add list formatting, adding button functionality, minor css changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $('#button-add-skill').click(function() {
     let skillEntered = $('#input-add-skill').val();
-    let list = $('#tbody');
-    let lineFormat = `<tr><td>${skillEntered}</td><br/></tr>`;
+    let list = $('tbody');
+    let lineFormat = `<tr><td class="list-items"><button class="button-remove-list-item">X</button>${skillEntered}</td><br/></tr>`;
     console.log(skillEntered);
     console.log(lineFormat);
-    // list.append(lineFormat)
+    list.append(lineFormat);
     // $('#tbody').append(`<tr><td>${skillEntered}</td><br/></tr>`);
 
 });
