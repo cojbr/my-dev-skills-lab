8789ebd5e384464a145101a6f7b3be1d11bac75a
Cody Breen <codybreen@Codys-MacBook-Air.local>
2019-12-09 00:42:58 -0500

    Troubleshoot remove button.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ $('#button-add-skill').click(function() {
     let skillEntered = $('#input-add-skill').val();
     let list = $('tbody');
     let lineFormat = `<tr><td class="list-items"><button class="button-remove-list-item">X</button>${skillEntered}</td><br/></tr>`;
-    console.log(skillEntered);
-    console.log(lineFormat);
     list.append(lineFormat);
-    // $('#tbody').append(`<tr><td>${skillEntered}</td><br/></tr>`);
-
 });
+
+
+$('.button-remove-list-item').click(function() {
+    let lineToRemove = $('.list-items').closest;
+    console.log(lineToRemove.val());
+    lineToRemove.remove();
+});
+
 
